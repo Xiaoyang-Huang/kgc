6ba613cb8e62bb9cb6b1500dcb422a8c1cb2ecf5
Brook Huang <brook@hp-workstation>
2017-02-22 17:14:05 +0800

    fix bugs

--- React Server Render/index.js
+++ React Server Render/index.js
@@ -1,20 +1,28 @@
 var React = require('react');
 var ReactDOMServer = require('react-dom/server');
+var ReactRouter = require('react-router');
+var match = ReactRouter.match;
 
+var Redux = require('redux');
+var Thunk = require('redux-thunk').default;
+
+var reducer = require('./script/reducer/index.js');
+
+var port = global.port = 3000;
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
-var port = 3000;
+var bodyParser = require('body-parser');
+
+app.use(require('connect-livereload')());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', express.static('www'));
+
+var Application = require('./www/dest/index.js');
 
 var DB = [];
 var idGen = 0;
 global.DB = DB;
-
-
-app.use(require('connect-livereload')());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use('/',express.static('www'));
 
 //api start
 app.post('/api/add', function(req, res){
@@ -43,12 +51,33 @@ app.post('/api/toggleAll', function(req, res){
 })
 //api end
 
-var Templete = require('./www/dest/index.js');
-
-app.get('/*', function(req, res){
-  var html = '<!DOCTYPE html>';
-  html += ReactDOMServer.renderToStaticMarkup(React.createElement(Templete, {URL: req.url, DB: DB}));
-  res.send(html);
+app.get('/*', function (req, res) {
+  match({ routes: Application.routes, location: req.url }, (error, redirectLocation, renderProps) => {
+    if (error) {
+      res.status(500).send(error.message)
+    } else if (redirectLocation) {
+      res.redirect(302, redirectLocation.pathname + redirectLocation.search)
+    } else if (renderProps) {
+      var route = renderProps.routes[renderProps.routes.length - 1];
+      var store = Redux.createStore(reducer, {todo: DB}, Redux.applyMiddleware(Thunk));
+      if (route.action) {
+        var asyncAction = route.action(store, req);
+        if (asyncAction) {
+          asyncAction.then(function () {
+            res.status(200).send(Application.template
+              .replace('{app}', ReactDOMServer.renderToString(React.createElement(Application.app, { URL: req.url, store: store, renderProps: renderProps })))
+              .replace('{serverStore}', 'window.__INITIAL_STORE__ = ' + JSON.stringify(store.getState()) + ';'));
+          }, function (err) { console.error(err); res.status(500).send(err) });
+          return;
+        }
+      }
+      res.status(200).send(Application.template
+        .replace('{app}', ReactDOMServer.renderToString(React.createElement(Application.app, { URL: req.url, store: store, renderProps: renderProps })))
+        .replace('{serverStore}', 'window.__INITIAL_STORE__ = ' + JSON.stringify(store.getState()) + ';'));
+    } else {
+      res.status(404).send('Not found')
+    }
+  })
 })
 
 app.listen(port);
--- React Server Render/script/templete.jsx
+++ React Server Render/script/templete.jsx
@@ -1,3 +1,6 @@
+var window = window || global;
+window.isServer = !window.document;
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Redux = require('redux');
@@ -5,64 +8,54 @@ var ReactRedux = require('react-redux');
 var ReactRouter = require('react-router');
 var Thunk = require('redux-thunk').default;
 
-var window = window || global;
-
-var Main = require('./index.jsx');
+var containerID = 'main';
 
 var Routes = require('./router.jsx');
+module.exports.routes = Routes;
 
-if(window.document){
+var reducer = require('./reducer/index.js');
 
-  var reducer = require('./reducer/todo.js');
+if(!isServer){
   var store = Redux.createStore(reducer, window.__INITIAL_STORE__ ? window.__INITIAL_STORE__ : {}, Redux.applyMiddleware(Thunk));
-  
-  ReactDOM.render(
-  <ReactRedux.Provider store={store}>
-    <ReactRouter.Router routes={Routes} history={ReactRouter.hashHistory} />
-  </ReactRedux.Provider>, document.getElementById('main'));
-
+  window.store = store;
+  window.route = Routes;
+  ReactRouter.match({history: ReactRouter.browserHistory, routes:Routes}, (error, redirectLocation, renderProps) => {
+    global.initial = true;
+    ReactDOM.render(<ReactRedux.Provider store={store}>
+      <ReactRouter.Router {...renderProps} />
+    </ReactRedux.Provider>, document.getElementById(containerID))
+    global.initial = false;
+  })
 }else{
-
-  var Templete = React.createClass({
-    render: function(){
-      var reducer = require('./reducer/todo.js');
-      var store = Redux.createStore(reducer, { todo: this.props.DB }, Redux.applyMiddleware(Thunk));
-
-      var context = {}
-      ReactRouter.match({ routes:Routes, location: this.props.URL }, (error, redirectLocation, renderProps) => {
-        context.error = error;
-        context.redirectLocation = redirectLocation;
-        context.renderProps = renderProps;
-      })
-
-      var content = null;
-      if(context.error){
-        content = <span>error!</span>
-      }else{
-        content = React.createElement(ReactRouter.RouterContext, context.renderProps);
-      }
-      
-      return (
-      <html>
+  
+  var Template = (
+  ` <!DOCTYPE html>
+    <html>
       <head>
-        <title>React Start Kit</title>
-        <link rel="stylesheet" type="text/css" href="dest/index.css" />
+        <title>React Server Render</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <link rel="stylesheet" type="text/css" href="/dest/index.css" />
       </head>
       <body>
-      <div id="main">
-        <ReactRedux.Provider store={store}>
-          {content}
-        </ReactRedux.Provider>
-      </div>
-      <script type="text/javascript" dangerouslySetInnerHTML={{__html:'window.__INITIAL_STORE__ = ' + JSON.stringify(store.getState()) + ';'}}></script>
-      <script type="text/javascript" src="dest/index.js"></script>
+      <div id={containerID} className="container-fluid">{app}</div>
+      <script type="text/javascript">{serverStore}</script>
+      <script type="text/javascript" src="/dest/index.js"></script>
       </body>
-      </html>
+    </html>
+  `);
+  Template = Template.replace('{containerID}', containerID);
+
+  var App = React.createClass({
+    render: function(){
+      return (
+        <ReactRedux.Provider store={this.props.store}>
+          <ReactRouter.RouterContext {...this.props.renderProps} />
+        </ReactRedux.Provider>
       )
     }
   })
 
-  module.exports = Templete;
+  module.exports.template = Template;
+  module.exports.app = App;
 }
-
 
