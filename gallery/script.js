aedc81c4bc97aafb1a473277e227bac843cce53c
Brook Huang <hxy881004@gmail.com>
2017-02-23 13:53:58 +0800

    fix format issue

--- gallery/script.js
+++ gallery/script.js
@@ -1,6 +1,6 @@
-function XyGallery(id, option){
+function XyGallery(id, option) {
   var container = document.getElementById(id);
-  if(!~container.className.indexOf('xy-gallery')) container.className += " xy-gallery";
+  if (!~container.className.indexOf('xy-gallery')) container.className += " xy-gallery";
   var defaultOption = {
     defaultWidth: 130,
     defaultHeight: 40,
@@ -12,16 +12,16 @@ function XyGallery(id, option){
   option = Object.assign({}, defaultOption, option);
   console.log(option)
 
-  if(option.width * option.height != container.children.length) throw "width and height not match children length!";
+  if (option.width * option.height != container.children.length) throw "width and height not match children length!";
   container.style.width = (option.width - 1) * option.defaultWidth + option.activeHeight + 'px'
 
   var lastRunTime = new Date(0);
   var runId = 0;
-  var activePicture = function(index){
+  var activePicture = function (index) {
     clearTimeout(runId)
     var currentTime = new Date();
-    if(currentTime - lastRunTime < option.animateDuration){
-      runId = setTimeout(function(){
+    if (currentTime - lastRunTime < option.animateDuration) {
+      runId = setTimeout(function () {
         activePicture(index)
       }, option.animateDuration);
       return;
@@ -29,23 +29,23 @@ function XyGallery(id, option){
     lastRunTime = currentTime;
     var cx = index % option.width;
     var cy = Math.floor(index / option.width);
-    for(var x=0,xl=option.width; x<xl; x++){
-      for(var y=0,yl=option.height; y<yl; y++){
+    for (var x = 0, xl = option.width; x < xl; x++) {
+      for (var y = 0, yl = option.height; y < yl; y++) {
         var cindex = y * option.width + x;
         var item = container.children[cindex];
-        if(x == cx && y == cy){
+        if (x == cx && y == cy) {
           item.className = "active";
           item.style.width = option.activeWidth + "px";
           item.style.height = option.activeHeight + "px";
-        }else if(x == cx){
+        } else if (x == cx) {
           item.className = "";
           item.style.width = option.activeWidth + "px";
           item.style.height = option.defaultHeight + "px";
-        }else if(y == cy){
+        } else if (y == cy) {
           item.className = "";
           item.style.width = option.defaultWidth + "px";
           item.style.height = option.activeHeight + "px";
-        }else{
+        } else {
           item.className = "";
           item.style.width = option.defaultWidth + "px";
           item.style.height = option.defaultHeight + "px";
@@ -56,8 +56,8 @@ function XyGallery(id, option){
   activePicture(0);
 
   var runId = 0;
-  Array.prototype.forEach.call(container.children, function(o, i){
-    o.addEventListener('mouseenter', function(evt){
+  Array.prototype.forEach.call(container.children, function (o, i) {
+    o.addEventListener('mouseenter', function (evt) {
       activePicture(i);
     })
   })
